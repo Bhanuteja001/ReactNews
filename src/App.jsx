3bca2b95524dbbf3a06235440ce68ccffb740204
Bhanuteja001 <bhanuchiluka234@gmail.com>
2024-06-22 11:49:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import Navbar from './Components/Navbar'
+import News from './Components/News'
+import { Routes,Route } from 'react-router-dom'
+import NewsItem from './Components/NewsItem'
+
+
+export default function App() {
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route  path='/' element={<News category="general" key="general"/>}/>
+      <Route  path='/food' element={<News  category="food" key="food"/>}/>
+      <Route  path='/politics' element={<News category="politics" key="politics"/>}/>
+      <Route  path='/movie' element={<News category="movie" key="movie"/>}/>
+      <Route  path='/sports' element={<News category="sports" key="sports"/>}/>    
+      <Route  path='/environment' element={<News category="environment" key="environment"/>}/>
+      <Route  path='/business' element={<News  category="business" key="business"/>}/>
+      <Route  path='/technology' element={<News  category="technology" key="technology"/>}/>    
+      <Route  path='/brief' element={<NewsItem/>}/>    
+    </Routes>
+    
+    </>
+  )
+}
+
+// business entertainment general health science sports technology
---
+++ src/Components/News.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import NewsCard from './NewsCard'
+import axios from 'axios'
+import Loader from './Loader'
+import { useSelector, useDispatch } from 'react-redux'
+import { list } from '../news/newsSlice'
+
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+function News(props) {
+  const [page, setpage] = useState(1)
+  const [loading, setLoading] = useState(true)
+  const { news } = useSelector((state) => state.news)
+  const dispatch = useDispatch()
+
+  const stateChange = (newNews) => {
+    dispatch(list({
+      news: newNews
+    }))
+  }
+
+  const url = `https://api.currentsapi.services/v1/latest-news?apiKey=BvrPMCHhsKvPkTVP7-wdqrFsCVNBDnB7jiLFKoN2nTKf4B1U&country=IN`
+
+  const getData = async () => {
+    setLoading(true)
+    try {
+      const res = await axios.get(`${url}&category=${props.category}&page_number=${page}&page_size=9`)
+      stateChange(res.data.news)
+      setLoading(false)
+    } catch (err) {
+      console.log(err)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [page])
+
+  return (
+    <>
+      {loading ? <Loader /> :
+       
+          <div className="container mt-5" style={{ width: "100%", margin: "20px auto" }}>
+            <h1 style={{textTransform:"capitalize"}}>{props.category}</h1>
+            <div className="row mx-auto d-flex justify-content-center" style={{ width: "99%", margin: "20px auto" }}>
+              {
+                news.map((item) => (
+                  <NewsCard key={item.id} content={item.description} imgurl={item.image} title={item.title} desc={item.description} />
+                ))
+              }
+            </div>
+            <div className='d-flex justify-content-between'>
+                    <button className="btn btn-primary" onClick={()=>setpage(page>1?page-1:page)} disabled={page==1}>&lt;</button>
+                    <button className="btn btn-primary" onClick={()=>setpage(page=>page+1)}>&gt;</button>
+                </div>
+          </div>
+        
+      }
+    </>
+  )
+}
+
+export default News
